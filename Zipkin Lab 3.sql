8bc00e2bdacebcc509a886a6734f69d3e9aa7043
Link0306 <hungryjosh@yahoo.com>
2014-02-06 21:41:36 -0500

    Lab 3: Getting started with SQL queries by Joshua Zipkin, 2/6/14

---
+++ Zipkin Lab 3.sql
@@ -0,0 +1,59 @@
+﻿--Joshua D. Zipkin
+
+--Query 1
+
+select name, city
+from agents
+where name = 'Smith'
+
+--Query 2
+
+select pid, name, quantity
+from products
+where priceUSD > 1.25
+
+--Query 3
+
+select ordno, aid
+from orders
+
+--Query 4
+
+select name, city
+from customers
+where city = 'Dallas'
+
+--Query 5
+
+select name
+from agents
+where city != 'New York'
+	and city != 'Newark'
+
+--Query 6
+
+select *
+from products
+where city != 'New York'
+	and city != 'Newark'
+	and priceUSD >= 1.00
+
+--Query 7
+
+select *
+from orders
+where mon = 'jan'
+	or mon = 'mar'
+
+--Query 8
+
+select *
+from orders
+where mon = 'feb'
+	and dollars < 100
+
+--Query 9
+
+select *
+from orders
+where cid = 'c001'
