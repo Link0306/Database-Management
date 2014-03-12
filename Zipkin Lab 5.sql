4b8a0cd77b46392d17ec80c71c7228617efb0b3d
Link0306 <hungryjosh@yahoo.com>
2014-03-12 13:58:43 -0400

    Lab 5: SQL Queries - The Three-quel Version 1 by Joshua Zipkin, 3/12/14

---
+++ Zipkin Lab 5.sql
@@ -0,0 +1,53 @@
+﻿--Joshua Zipkin
+
+--Query 1
+
+select agents.city
+from customers, agents, orders
+where customers.cid = orders.cid
+  and agents.aid = orders.aid
+  and customers.name = 'Basics'
+order by city asc;
+
+--Query 2
+
+select *
+from customers c
+inner join orders o
+   on c.cid = o.cid;
+   
+
+--Query 3
+
+select name
+from customers
+where cid not in (select cid
+		  from orders);
+
+--Query 4
+
+select c.name
+from customers c
+left outer join orders o
+  on c.cid = o.cid
+where o.cid is null;
+
+--Query 5
+
+select *
+from customers c, agents a, orders o
+where c.cid = o.cid
+  and a.aid = o.aid
+
+--Query 6
+
+select c.name, a.name, c.city
+from customers c, agents a
+where c.city = a.city;
+
+--Query 7
+
+select c.name, c.city
+from customers c, products p
+where c.city = p.city
+group by c.name, c.city;
