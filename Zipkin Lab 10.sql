a1c6172458171b7c4812a5b372e8e85ec4204acf
Link0306 <hungryjosh@yahoo.com>
2014-04-25 00:12:44 -0400

    Lab 10: Stored Procedures by Joshua Zipkin, 4/25/14

---
+++ Zipkin Lab 10.sql
@@ -0,0 +1,38 @@
+﻿--Joshua Zipkin
+--Lab 10
+
+--PreReqsFor
+create or replace function PreReqsFor(int, REFCURSOR) returns refcursor as 
+$$
+declare
+   inputNum  int       := $1;
+   resultset REFCURSOR := $2;
+begin
+   open resultset for 
+      select num, name, credits
+      from courses
+      where num in (select preReqNum
+                    from prerequisites
+                    where courseNum = inputNum);
+   return resultset;
+end;
+$$ 
+language plpgsql;
+
+--isPreReqFor
+create or replace function isPreReqFor(int, REFCURSOR) returns refcursor as 
+$$
+declare
+   inputNum  int       := $1;
+   resultset REFCURSOR := $2;
+begin
+   open resultset for 
+      select num, name, credits
+      from courses
+      where num in (select courseNum
+                    from prerequisites
+                    where preReqNum = inputNum);
+   return resultset;
+end;
+$$ 
+language plpgsql;
